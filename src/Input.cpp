517d471455a9676c44d65883d04e6c7dc5ed0e0c
jnordin20 <jnordin@fyusion.com>
2019-06-15 16:58:18 -0700

    Initial commit

---
+++ src/Input.cpp
@@ -0,0 +1,21 @@
+namespace NS {
+
+Foo::~Foo() {}
+
+Foo::Size() const { return 4; }
+
+void Foo::SetBar(int i) { bar = i; }
+
+void Foo::Frobnicate()
+{
+    doSomething();
+    doSomethingElse();
+}
+
+void Foo::ReverseFrobnicate()
+{
+    doSomethingElse();
+    doSomething();
+}
+
+}  // ::NS
